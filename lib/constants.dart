5f175f7db91c867c641ff413c36975ffff768699
Amr Turki <amrturki79@gmail.com>
2026-03-24 10:40:06 +0200

    finish notes bottom sheet

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
 import 'package:flutter/material.dart';
 
-const KPrimaryColor = Colors.red;
+const KPrimaryColor = Colors.blue;
--- lib/widget/add_note_bottom_sheet.dart
+++ lib/widget/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widget/custom_button.dart';
 import 'package:notes_app/widget/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,13 +9,16 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 16.0, right: 16),
-      child: Container(
+      child: SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(height: 35),
             CustomTextField(hint: 'title'),
             SizedBox(height: 25),
             CustomTextField(hint: 'content', maxlines: 5),
+            SizedBox(height: 25),
+            CustomButton(),
+            SizedBox(height: 15),
           ],
         ),
       ),
