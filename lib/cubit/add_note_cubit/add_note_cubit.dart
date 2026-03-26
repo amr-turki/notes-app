fa154cf54fbb993340c668b47a227e5be0ea05fa
Amr Turki <amrturki79@gmail.com>
2026-03-26 13:36:29 +0200

    add note cubit and state

---
+++ lib/cubit/add_note_cubit/add_note_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+part 'add_note_state.dart';
+
+class NotesCubit extends Cubit<AddNoteState> {
+  NotesCubit() : super(AddNoteInitial());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/model/note_model.dart';
 import 'package:notes_app/screen/notes_screen.dart';
 
 void main() async {
